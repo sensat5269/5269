3d8dcd1b92179738e6cdf708282bbe00e0f71242
sensat5269 <79953011+sensat5269@users.noreply.github.com>
2021-03-03 10:46:08 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+''' main '''
+# 使用自建的套件 myMath 來計算使用者輸入的五個數的平均值
+from myMath import myStatistics
+ass=[]
+for  i in range(1,6):
+    ass.append(float(input('請輸入5個數以計算平均數，第'+str(i)+'個：')))
+  
+print(myStatistics.myMean(ass))
